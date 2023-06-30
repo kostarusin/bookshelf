fe7019a16c23fd6ccae418147cc249fda23f2ad3
Viktoria <viktoria.ttravel@gmail.com>
2023-06-30 20:52:48 +0300

    categories 3

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,15 +1,37 @@
 import categories from '../../categories_list.json';
-console.log(categories[0]);
-console.dir(categories);
+import top_books from '../../top_books.json';
+// console.log(categories[0]);
+// console.dir(categories);
+console.dir(top_books[0]);
 
 const listCategoriesEl = document.querySelector('.list-categories');
 
 //console.log(typeof []);
 for (const category of categories) {
   let item = document.createElement('li');
-  item.textContent = category.list_name;
+  item.classList.add('item-categories');
+  //const itemCategoriesEl = document.querySelector('.item-categories');
+  let link = document.createElement('a');
+  link.textContent = category.list_name;
+  link.setAttribute('href', '#');
+  item.append(link);
   listCategoriesEl.append(item);
-  // <a class="menu-link" href="./index.html">
-  //   Studio
-  // </a>;
 }
+
+listCategoriesEl.addEventListener('click', event => {
+  if (event.target.nodeName === 'A') {
+    const textCategory = event.target.textContent;
+    //console.log(textCategory);
+    //console.log(event.target);
+    toUpperCase(textCategory);
+  }
+});
+
+const toUpperCase = function (textCategory) {
+  const normalizedToUpperCase = textCategory.toUpperCase();
+  console.log(normalizedToUpperCase);
+};
+
+// for (const topBook of top_books) {
+
+// }
