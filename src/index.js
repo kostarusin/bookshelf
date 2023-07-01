cd21cff25a09c1393100d5cb4a2309efad73fbca
Viktoria <viktoria.ttravel@gmail.com>
2023-07-01 18:54:36 +0300

    categories 9

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './js/loader';
 import './js/render';
 import './js/services';
 import './js/storage';
+import './js/theme';
+import './js/scroll-up';
 import './js/support_ukraine';
 import './js/scroll-up';
 import './js/categories';
