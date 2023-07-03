f7187d9d9aa2dd75b8364d1e0d6d2786055e6b3d
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-07-03 11:26:47 +0300

    update

--- src/js/support_ukraine.js
+++ src/js/support_ukraine.js
@@ -1,50 +1,50 @@
 import array from './static-json-temporary/support_ukraine.json';
-let array=[{
-  "title": "Save the Children",
-  "url": "https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis",
-  "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/save-the-children.png?raw=true"
-},
-{
-  "title": "Project HOPE",
-  "url": "https://www.projecthope.org/country/ukraine/",
-  "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/hope.png?raw=true"
-},
-{
-  "title": "International Medical Corps",
-  "url": "https://internationalmedicalcorps.org/country/ukraine/",
-  "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/international-medical-corps.png?raw=true"
-},
-{
-  "title": "RAZOM",
-  "url": "https://www.razomforukraine.org/",
-  "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/razom.png?raw=true"
-},
-{
-  "title": "UNITED24",
-  "url": "https://u24.gov.ua/uk",
-  "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/united24.png?raw=true"
-},
-{
-  "title": "Medicins Sans Frontieres",
-  "url": "https://www.msf.org/ukraine",
-  "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/medicins-sans-frontieres.png?raw=true"
-},
-{
-  "title": "Action against hunger",
-  "url": "https://www.actionagainsthunger.org/location/europe/ukraine/",
-  "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/action-against-hunger.png?raw=true"
-},
-{
-  "title": "World vision",
-  "url": "https://www.wvi.org/emergencies/ukraine",
-  "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/world-vision.png?raw=true"
-},
-{
-  "title": "Serhiy Prytula Charity Foundation",
-  "url": "https://prytulafoundation.org/en",
-  "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/sergiy-prytula.png?raw=true"
-}
-]
+// let array=[{
+//   "title": "Save the Children",
+//   "url": "https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis",
+//   "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/save-the-children.png?raw=true"
+// },
+// {
+//   "title": "Project HOPE",
+//   "url": "https://www.projecthope.org/country/ukraine/",
+//   "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/hope.png?raw=true"
+// },
+// {
+//   "title": "International Medical Corps",
+//   "url": "https://internationalmedicalcorps.org/country/ukraine/",
+//   "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/international-medical-corps.png?raw=true"
+// },
+// {
+//   "title": "RAZOM",
+//   "url": "https://www.razomforukraine.org/",
+//   "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/razom.png?raw=true"
+// },
+// {
+//   "title": "UNITED24",
+//   "url": "https://u24.gov.ua/uk",
+//   "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/united24.png?raw=true"
+// },
+// {
+//   "title": "Medicins Sans Frontieres",
+//   "url": "https://www.msf.org/ukraine",
+//   "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/medicins-sans-frontieres.png?raw=true"
+// },
+// {
+//   "title": "Action against hunger",
+//   "url": "https://www.actionagainsthunger.org/location/europe/ukraine/",
+//   "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/action-against-hunger.png?raw=true"
+// },
+// {
+//   "title": "World vision",
+//   "url": "https://www.wvi.org/emergencies/ukraine",
+//   "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/world-vision.png?raw=true"
+// },
+// {
+//   "title": "Serhiy Prytula Charity Foundation",
+//   "url": "https://prytulafoundation.org/en",
+//   "img": "https://github.com/AlexGoIT/images/blob/b54b19358d2d9e95d2472b43b54376b1ef1dc3e9/support-icons/sergiy-prytula.png?raw=true"
+// }
+// ]
 
   const divEl =document.querySelector('.slider_line');
   const olEl=document.querySelector('.ol_support');
