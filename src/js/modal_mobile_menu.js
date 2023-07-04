35b1bdf50dcc0d962a384f45f14e6e2c3dd5ec75
goldenkaia <julia_tserkovna@gmx.net>
2023-07-04 15:01:47 +0200

    modal-menu-buttons added

--- src/js/modal_mobile_menu.js
+++ src/js/modal_mobile_menu.js
@@ -1,10 +1,14 @@
 const menuBtnEl = document.querySelector('.header-burger');
 const menuBoxEl = document.querySelector('.js-box-menu');
 const bodyEl = document.body;
+const iconBurgerMenuEl = menuBtnEl.querySelector('.icon-burger-menu');
+const iconCloseEl = menuBtnEl.querySelector('.back-to-shoping-list-btn-icon');
 
 menuBtnEl.addEventListener('click', onClickMenuBtn);
 
 function onClickMenuBtn() {
+  iconCloseEl.classList.toggle('is-hidden');
+  iconBurgerMenuEl.classList.toggle('is-hidden');
   menuBoxEl.classList.toggle('show');
   bodyEl.classList.toggle('no-scroll');
 }
