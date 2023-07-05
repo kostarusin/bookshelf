4388839ef0409767cda38998222d2cd0f7be8779
boomsik <pozdnyakov151994@gmail.com>
2023-07-05 23:18:24 +0300

    best commit

--- src/js/modal-authorization.js
+++ src/js/modal-authorization.js
@@ -3,14 +3,14 @@
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open-desk]"),
     closeModalBtn: document.querySelector("[data-modal-close-desk]"),
-    modal: document.querySelector("[data-modal]"),
+    modal: document.querySelector("[data-modal-new]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle("is-hiddens");
   }
 })();
 
@@ -20,13 +20,13 @@
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open-mobile]"),
     closeModalBtn: document.querySelector("[data-modal-close-mobile]"),
-    modal: document.querySelector("[data-modal]"),
+    modal: document.querySelector("[data-modal-new]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle("is-hiddens");
   }
 })();
